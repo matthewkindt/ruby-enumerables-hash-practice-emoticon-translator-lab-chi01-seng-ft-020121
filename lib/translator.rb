3d2df5df11c09ef8259df780218fb5847a1de72c
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-12 18:42:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,6 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
 end
